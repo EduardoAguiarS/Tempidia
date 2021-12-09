5e3f8c3a23d5534ce1bd8129628bad67dd42825c
Eduardo Aguiar <edu.pessoal@outlook.com>
2021-12-08 23:40:18 -0300

    gif portrait

--- js/functions/temtem_portrait.js
+++ js/functions/temtem_portrait.js
@@ -1,7 +1,11 @@
 import { temtemImg, temtemName } from "../app.js";
 
 function temtemPortrait(name, number, temtemDescription) {
-  temtemImg.src = `https://temtem-api.mael.tech/images/portraits/temtem/large/${name}.png`;
+  temtemImg.setAttribute(
+    "onerror",
+    `this.src='https://temtem-api.mael.tech/images/portraits/temtem/large/${name}.png'`
+  );
+  temtemImg.src = `https://temtem-api.mael.tech/images/renders/temtem/animated/${name}.gif`;
   temtemName.textContent = name;
   description.innerHTML = `<p>Description: <span>${temtemDescription}</span></p>`;
   temtemId.innerHTML = `<p>Number: <span>${number}</span></p>`;
