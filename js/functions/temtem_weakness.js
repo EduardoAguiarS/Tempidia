24fb3c8fe0f4055ddb7da93a377366f0fc3b1656
Eduardo Aguiar <edu.pessoal@outlook.com>
2021-12-10 10:05:00 -0300

    wea ness

--- js/functions/temtem_weakness.js
+++ js/functions/temtem_weakness.js
@@ -7,6 +7,7 @@ export function calcWeak(type) {
     .then(response => response.json())
     .then(data => {
       type.forEach((item, index, array) => {
+        // Create Damge img
         if (array[0] == item) {
           const createImg = document.createElement("img");
           createImg.src = `https://temtem-api.mael.tech/images/icons/types/${item}.png`;
@@ -19,6 +20,21 @@ export function calcWeak(type) {
           createImg.src = `https://temtem-api.mael.tech/images/icons/types/${item}.png`;
           createImg.classList.add("weakness-img");
           matchup2.before(createImg);
+        }
+
+        //Create Weakness img
+        if (array[0] == item) {
+          const createImg = document.createElement("img");
+          createImg.src = `https://temtem-api.mael.tech/images/icons/types/${item}.png`;
+          createImg.classList.add("weakness-img");
+          weakness1.before(createImg);
+        }
+
+        if (array[1] == item) {
+          const createImg = document.createElement("img");
+          createImg.src = `https://temtem-api.mael.tech/images/icons/types/${item}.png`;
+          createImg.classList.add("weakness-img");
+          weakness2.before(createImg);
         }
 
         for (let types in data) {
@@ -43,11 +59,31 @@ export function calcWeak(type) {
           //   `${types} x ${item}: ${temtemWeak[item]}x damage received`
           // );
 
-          // if (temtemWeak[item] == 2) {
-          //   console.log(
-          //     `${types} x ${item}: ${temtemWeak[array[index]]}x damage received`
-          //   );
-          // }
+          if (temtemWeak[item] == 2) {
+            console.log(
+              `${types} x ${item}: ${temtemWeak[array[index]]}x damage received`
+            );
+          }
+
+          if (temtemWeak[item] == 2) {
+            if (array[0] == item) {
+              const createLi = document.createElement("li");
+              weakness1.appendChild(createLi);
+              createLi.classList.add("weakness");
+              createLi.innerHTML = `<img src="https://temtem-api.mael.tech/images/icons/types/${types}.png"> <span>${
+                temtemWeak[array[index]]
+              }</span> `;
+            }
+
+            if (array[1] == item) {
+              const createLi = document.createElement("li");
+              weakness2.appendChild(createLi);
+              createLi.classList.add("weakness");
+              createLi.innerHTML = `<img src="https://temtem-api.mael.tech/images/icons/types/${types}.png"> <span>${
+                temtemWeak[array[index]]
+              }</span> `;
+            }
+          }
         }
       });
     });
