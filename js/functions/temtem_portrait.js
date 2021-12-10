4c41813064d9c96142dd66e49083826daf38caee
Eduardo Aguiar <edu.pessoal@outlook.com>
2021-12-10 09:39:52 -0300

    não mostra mais gifs

--- js/functions/temtem_portrait.js
+++ js/functions/temtem_portrait.js
@@ -1,11 +1,7 @@
 import { temtemImg, temtemName } from "../app.js";
 
 function temtemPortrait(name, number, temtemDescription) {
-  temtemImg.setAttribute(
-    "onerror",
-    `this.src='https://temtem-api.mael.tech/images/portraits/temtem/large/${name}.png'`
-  );
-  temtemImg.src = `https://temtem-api.mael.tech/images/renders/temtem/animated/${name}.gif`;
+  temtemImg.src = `https://temtem-api.mael.tech/images/portraits/temtem/large/${name}.png`;
   temtemName.textContent = name;
   description.innerHTML = `<p>Description: <span>${temtemDescription}</span></p>`;
   temtemId.innerHTML = `<p>Number: <span>${number}</span></p>`;
